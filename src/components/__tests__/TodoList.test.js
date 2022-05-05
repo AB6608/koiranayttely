50125ba93099002d09f6f7ba169cb678d0553289
Your Name <ab6608@student.jamk.fi>
2022-05-05 21:24:31 +0300

    heroku

--- src/components/__tests__/TodoList.test.js
+++ src/components/__tests__/TodoList.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import TodoList from '../TodoList';
 
 test('should render TodoList component', () => {
-    render(<TodoList/>);
-    const todoListElement = screen.getByTestId('todo-list');
-    expect(todoListElement).toBeInTheDocument();
+  render(<TodoList />);
+  const todoListElement = screen.getByTestId('todo-list');
+  expect(todoListElement).toBeInTheDocument();
 });
